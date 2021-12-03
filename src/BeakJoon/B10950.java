1da4c83fc43864689a13010001fac37adb8fb7ce
Teddy <ahn3752@naver.com>
2021-12-04 00:16:11 +0900

    2021-12-04 B10950 commit

--- src/BeakJoon/B10950.java
+++ src/BeakJoon/B10950.java
@@ -15,8 +15,9 @@ public class B10950 {
         for(int i=0; i< num; i++){
 
             String AB = bf.readLine();
+            //readLine은 한 행을 전부 읽기때문에 공백단위로 입력한 문자열을 분리 해주어야 한다.
             StringTokenizer st = new StringTokenizer(AB, " ");
-
+            //StringTokenizer 클래스 를 이용해 분리 해준다.
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int result = A+B;
